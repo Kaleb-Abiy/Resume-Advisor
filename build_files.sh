8b5f40b9abe1d148f1cfb86918d58d3c288a122a
kaleb <kalebabiy2012@gmail.com>
2024-06-17 17:08:55 +0300

    fix dependency issues

--- build_files.sh
+++ build_files.sh
@@ -6,10 +6,20 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
+pip3.9 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
+
 # install all deps in the venv
 pip install -r requirements.txt
 
+# spaCy
+python3.9 -m spacy download en_core_web_sm
+
+# nltk
+python3.9 -m nltk.downloader words
+python3.9 -m nltk.downloader stopwords
+
+pip3.9 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
 # collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 echo "BUILD END"
