1719dc8a33658ec8c36a54a2870f98c9df163a14
kaleb <kalebabiy2012@gmail.com>
2024-06-18 09:27:11 +0300

    new chages made

--- build.sh
+++ build.sh
@@ -4,46 +4,28 @@ set -o errexit
 
 echo "BUILD START"
 
-pip install nltk
+pip install -r requirements.txt
 
 python -m nltk.downloader words
 python -m nltk.downloader stopwords
 
-# create a virtual environment named 'venv' if it doesn't already exist
+# spaCy
+python -m spacy download en_core_web_sm
 
-# activate the virtual environment
-
-pip install --upgrade pip
-
-pip install django
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
 
 
-
-
+# pip install django
 
 pip install numpy==1.25
-
-
-pip install pyresparser
+# pip install pyresparser
 
 # nltk
 
 
 pip install python-dotenv 
 
-# spaCy
-python -m spacy download en_core_web_sm
-
-
-
-
-
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
-
 pip install -q -U google-generativeai
-
-pip install -r requirements.txt
-
 
 
 python manage.py collectstatic --noinput
